c63900f5dcae8000f78b836341e45884e6307623
Toni Babenko <a.babenko.e@gmail.com>
2021-11-13 19:57:06 +0100

    change tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "212205595",
+        trackingId: "G-9XR5144PKV",
       },
     },
     "gatsby-plugin-react-helmet",
