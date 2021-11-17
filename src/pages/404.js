b24417232549f0d859b9e141435e3a5dedaa98bb
Toni Babenko <a.babenko.e@gmail.com>
2021-11-17 21:35:38 +0100

    fix favicons paths, update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,29 @@
-import * as React from "react"
+import * as React from "react";
+import Layout from "../components/layout";
+import styled from "styled-components";
+import {Helmet} from "react-helmet";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Message = styled.div`
+  margin-top: -100px;
+  font-size: 48px;
+`;
 
 const NotFoundPage = () => {
   return (
-    <main>
-      404
-    </main>
-  )
-}
+    <Layout>
+      <Helmet title="404"/>
+      <Wrapper>
+        <Message>404 | (ಥ﹏ಥ)</Message>
+      </Wrapper>
+    </Layout>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
